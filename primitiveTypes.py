88af1f3943567ded7bd7542a48519f839be2624a
hansn <hansn@cs.ubc.ca>
2020-12-07 16:23:22 -0500

    improved parity with xor

--- primitiveTypes.py
+++ primitiveTypes.py
@@ -9,8 +9,23 @@ def count_bits(x):
         x>>=1 # shift by 1 bit and do again
     return num_bits
 
+def parity_bits(x):
+    # calculate parity of a word
+    result = 0
+    while x:
+        result^=x&1 # first checks if lowest set bit is 1, then xor with 0 or 1 (switches between 0 and 1, mod 2)
+        x>>=1
+    return result # time complexity O(n)
+
+def improved_parity(x):
+    result = 0
+    while x:
+        result ^= 1
+        x &= (x - 1) # drop lowest set bit (basically drop bits k times, reduced time complexity is O(k)
+    return result
+
 
 if __name__ == '__main__':
-    # for first 5 integers, print binary representation and count 1s
+    # for first 5 integers, print binary representation and count 1s, print parity too
     for i in range(5):
-        print(i, bin(i)[2:].zfill(4), count_bits(i))
+        print(i, bin(i)[2:].zfill(4), count_bits(i), parity_bits(i), improved_parity(i))
