013a4ccc26a28e90fc2032eb6683418dc8120266
hansn <hansn@cs.ubc.ca>
2020-12-08 22:22:03 -0500

    some array fun implementation of quicksort with a timer

--- Arrays.py
+++ Arrays.py
@@ -3,16 +3,15 @@ from timer import Timer
 import numpy as np
 
 def quicksort(input_array: np.array, lo: int, hi: int):
-    # move pivot to end of array
     if lo < hi:
         p = partition(input_array, lo, hi)
         quicksort(input_array, lo, p - 1)
-        quicksort(input_array, p+1, hi)
+        quicksort(input_array, p + 1, hi)
 
 def partition(arr: np.array, lo: int, hi: int):
     i = (lo - 1)
     pivot = A[hi]
-    for j in range(lo,hi):
+    for j in range(lo, hi):
         if arr[j] <= pivot:
             i += 1
             arr[i], arr[j] = arr[j], arr[i]
@@ -22,7 +21,6 @@ def partition(arr: np.array, lo: int, hi: int):
 
 if __name__ == '__main__':
     t = Timer()
-
     t.start()
     A = np.random.randint(100, size=100)
     print(A)
